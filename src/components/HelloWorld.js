c35fb00dee1f30e78bad2c8447ecfffae3769684
Celestin Mbuyamba <celembuyamba@gmail.com>
2021-01-05 13:24:57 -0500

    Removed FreshPay request

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -32,7 +32,7 @@ const freshPayConfig = {
 
 axios.post("https://dev.makuta.cash/auth/login", makutaUser, makutaConfig).then(response => console.log(response.data));
 
-axios.post("https://146.148.10.8/api/v5", freshPayData, freshPayConfig).then(response => console.log(response.data));
+//axios.post("https://146.148.10.8/api/v5", freshPayData, freshPayConfig).then(response => console.log(response.data));
 
 const HelloWorld = (props) =>  <div style={{width: "40%", margin: "auto"}}>
 		<img style={{width: '100%'}} src={logo} alt=" "></img>
